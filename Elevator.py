699d563f74eb6edf585d739479aca841ff1a33a7
AryanThomare <aryanthomare@gmail.com>
2020-11-02 11:32:29 -0500

    Current 2

--- Elevator.py
+++ Elevator.py
@@ -9,11 +9,16 @@ screen = pygame.display.set_mode((width,height))
 yellow = (255,255,102)
 grey = (100,100,100)
 
+fpsClock = pygame.time.Clock()
 
 colors = [grey,yellow]
 
+floors = []
+for x in range(10):
+    floors.append(0)
+selected = 1
 buttonspress = []
-for x in range(18):
+for x in range(10):
     buttonspress.append([0,0])
 
 def generalbkg():
@@ -27,7 +32,7 @@ def generalbkg():
     empty_rect = pygame.Rect(460, 10, 200, 700)
     pygame.draw.rect(screen, (0, 0, 0), empty_rect, 3)
 
-def flnums(buttonspress):
+def flnums(buttonspress,selected):
     count = 1
     for y in range(700,0,-70):
         if buttonspress[count-1][0] == 0 and buttonspress[count-1][1] == 0:
@@ -35,26 +40,50 @@ def flnums(buttonspress):
         if buttonspress[count-1] == 1 or buttonspress[count-1] == 1:
             pygame.draw.rect(screen,colors[1],(35,720-y,70,35))
 
+
         font = pygame.font.Font('freesansbold.ttf', 45)
-        text = font.render(f"{count}", False, (0, 0, 0))
-        if count != 10:
+        if count-1 == 10-selected:
+            text = font.render(f"{11-count}", False, (0, 255, 0))
+        else:
+            text = font.render(f"{11-count}", False, (0, 0, 0))
+
+        if 11-count != 10:
             screen.blit(text, (55, 720 - y))
         else:
             screen.blit(text, (40, 720 - y))
-
-
-
         count += 1
+
+
 
 #def buttons():
     #for y in range()
 
 while True:
+    xcord, ycord = pygame.mouse.get_pos()
+    print (selected)
+
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == ord('w'):
+                if selected < 10:
+                    selected += 1
+            if event.key == ord('s'):
+                if selected > 1:
+                    selected -= 1
+            if event.key == pygame.K_UP:
+                buttonspress[10-selected][0] = 1
+            if event.key == pygame.K_DOWN:
+                buttonspress[10-selected][1] = 1
     generalbkg()
-    flnums(buttonspress)
+    flnums(buttonspress,selected)
 
     pygame.display.update()
+    fpsClock.tick(10)
+    print(buttonspress)
+pygame.quit()
+
+
